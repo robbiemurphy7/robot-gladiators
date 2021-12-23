5db9eaed1d88e684364e6b0f760d98a636f3fd01
robbiemurphy <rmurphy987@gmail.com>
2021-12-22 18:10:14 -0600

    fight() functions added

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,19 +1,32 @@
-// PROMPTS ROBOT NAME WINDOW, VAR CAPTURES PLAYER'S DATA
-
+// PROMPTS ROBOT NAMING WINDOW, VAR CAPTURES PLAYER'S DATA
 var playerName = window.prompt("What is your robot's name?");
-// NOTE THE LACK OF QUOTATION MARKS AROUND playerName
-console.log(playerName);
+var playerHealth = 100;
+var playerAttack = 10;
 
-console.log("This logs a string, good for leaving yourself a message");
-// THIS WILL DO MATH AND LOG 20
-console.log(10 + 10);
-// WHAT IS THIS?
-console.log("Our robot's name is " + playerName);
+// YOU CAN ALSO LOG MULTIPLE VALUES AT ONCE LIKE THIS console.log(playerName, playerAttack, playerHealth);
 
-// THIS CREATES A FUNCTION NAMED "FIGHT"
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
 
-function fight() {
-    window.alert("The fight has begun!");
-}
+var fight = function() {
+    // ALERT PLAYERS THAT THEY ARE STARTING THE ROUND
+    window.alert("Welcome to Robot Gladiators!");
 
-// fight();
+    // SUBTRACT THE VALUE OF 'playerAttack' FROM THE VALUE OF 'enemyHealth' AND USE THAT RESULT TO UPDATE THE VALUE IN THE 'enemyHealth' VARIABLE
+    enemyHealth = enemyHealth - playerAttack;
+
+    // SUBTRACT THE VALUE OF 'enemyAttack' FROM THE VALUE OF 'playerHealth' AND USE THAT RESULT TO UPDATE THE VALUE IN THE 'playerHealth' VARIABLE
+    playerHealth = playerHealth - enemyAttack;
+
+    // LOG A RESULTING MESSAGE TO THE CONSOLE SO WE KNOW THAT IT WORKED
+    console.log(
+    playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining.");
+
+    // LOG A RESULTING MESSAGE TO THE CONSOLE SO WE KNOW THAT IT WORKED
+    console.log(
+        enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remining.");
+        
+};
+
+fight();
